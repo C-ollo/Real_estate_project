7e3dd9eef154cc549bd6fc829b1137066aaedfff
Collins Kimutai <collinskimutai72@gmail.com>
2023-12-04 09:30:01 +0300

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInFailure,signInSuccess } from '../redux/user/userSlice';
+
 
 export default function Signin() {
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const {loading, error} = useSelector((state)=> state.user);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const handleChange = (e)=> {
         setFormData(
             {
@@ -16,7 +19,7 @@ export default function Signin() {
     };
     const handleSubmit = async (e)=>{
         try {
-                setLoading(true);
+                dispatch(signInStart());
             e.preventDefault();
             const res = await fetch('/api/auth/signin',{
                 method: 'POST',
@@ -27,20 +30,16 @@ export default function Signin() {
         });
         const data = await res.json();
         if (data.success === false) {
-            setError(data.message);
-            setLoading(false);
+            dispatch(signInFailure(data.message))
             return;
         }
-        setLoading(false);
-        setError(null);
+        dispatch(signInSuccess(data.message))
         navigate('/');
         } 
         catch (error) {
-            setLoading(false);
-            setError(error.message);
+            dispatch(signInFailure(error.message))
         }
         
-    
 };
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -54,7 +53,7 @@ export default function Signin() {
         <p>
            New User? 
         </p>
-        <Link to={"/signup"}>
+        <Link to={"/sign-up"}>
             <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
