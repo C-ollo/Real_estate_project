01f7a42d81b9478869be392485eaa8c6fab18c05
Collins Kimutai <collinskimutai72@gmail.com>
2023-12-06 08:06:49 +0300

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,32 +1,37 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRoute from './routes/user.route.js';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+// import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("Database connected successfully");
-    })
-    .catch((err) =>{
-        console.error(err);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('====================================');
-    console.log("Server is running on port 3000");
-    console.log('====================================');
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-app.use(express.json());
-app.get('/',(req,res)=>{
-    res.send('Wewlcome home')
-});
-app.use('/api/auth',authRouter);
-app.use('/api/user/test',userRoute);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+// app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { test } from '../contollers/user.contoller.js';
+import { test, updateUser } from '../contollers/user.contoller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 const userRoute = express.Router();
 
 
 userRoute.get('/',test)
+userRoute.post('/update/:id', verifyToken, updateUser)
 
 export default userRoute;
