b1b769fad4969a37360f5ef8d29d9cf81edbf49a
Collins Kimutai <collinskimutai72@gmail.com>
2023-12-06 07:27:16 +0300

    complete profile page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,8 @@ import About from './pages/About';
 import Header from './components/Header';
 import Signup from './pages/Signup';
 import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './pages/CreateListing';
-import UpdateListing from './pages/UpdateListing';
+// import CreateListing from './pages/CreateListing';
+// import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return (
@@ -20,11 +20,11 @@ export default function App() {
         <Route path="/sign-in" element={<Signin/>}/>
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing />} />
-          <Route
+          {/* <Route path='/create-listing' element={<CreateListing />} /> */}
+          {/* <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
-          />
+          /> */}
         </Route>
         <Route path="/sign-out" element={<Signout/>}/>
         <Route path="/sign-up" element={<Signup/>}/>
