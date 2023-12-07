22405adf65f598dec692b2b8ee6f88ea3d3e2e04
Collins Kimutai <collinskimutai72@gmail.com>
2023-12-07 10:47:30 +0300

    add edit listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing } from '../contollers/listing.contoller.js';
+import { createListing, deleteListing, updateListing } from '../contollers/listing.contoller.js';
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 export default router;
