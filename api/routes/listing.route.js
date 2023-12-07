51cfb91eb9af2178478af403b2854d33e91b29eb
Collins Kimutai <collinskimutai72@gmail.com>
2023-12-07 10:35:32 +0300

    comlete user listing delete functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing } from '../contollers/listing.contoller.js';
+import { createListing, deleteListing } from '../contollers/listing.contoller.js';
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing);
-
+router.delete('/delete/:id', verifyToken, deleteListing);
 export default router;
