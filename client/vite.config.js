a3873af49b0942a43ca244fdb7cae3d1ccd8480c
Collins Kimutai <collinskimutai72@gmail.com>
2024-01-04 20:14:29 +0300

    update for deployment to AWS

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     host: true,
-    port: 3000,
+    port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://18.202.28.95:3000',
         secure: false,
       },
     },
